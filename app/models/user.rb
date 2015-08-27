6c1494677bec895003533e6fa8d13d9042638a1c
ming <ming@stash>
2015-08-27 18:10:10 +0800

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
- validates :name, presence: true
+ validates :name, presence: true,length: { maximum: 50 }
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ validates :email, presence: true, length: { maximum: 255 },
+#validates :password, length: { maximum: 6 },
+ format: { with: VALID_EMAIL_REGEX },
+ uniqueness: true
+ has_secure_password
+ validates :password, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821030656) do
+ActiveRecord::Schema.define(version: 20150827092343) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  create_table "usertests", id: false, force: :cascade do |t|
+    t.integer  "id",         limit: 4,   default: 0, null: false
     t.string   "name",       limit: 255
     t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
 end
