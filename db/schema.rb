3166ffe38089805b409ad56f016c190bf1dc4ab5
ming <ming@stash>
2015-09-02 18:54:41 +0800

    902

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150827092343) do
+ActiveRecord::Schema.define(version: 20150902090037) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -19,16 +19,18 @@ ActiveRecord::Schema.define(version: 20150827092343) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
+    t.string   "remember_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "usertests", id: false, force: :cascade do |t|
-    t.integer  "id",         limit: 4,   default: 0, null: false
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.integer  "id",              limit: 4,   default: 0, null: false
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
